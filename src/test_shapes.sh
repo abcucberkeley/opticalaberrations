ce197fb599105af42dddc9c54f993f2f7b69be52
Thayer <thayeralshaabi@gmail.com>
2022-07-22 12:44:37 -0700

    testing with all sim objects

--- src/test_shapes.sh
+++ src/test_shapes.sh
@@ -15,7 +15,11 @@ for MODEL in "${models[@]}"
 do
   for NA in 1 .95 .9 .85 .8
   do
-    for REF in 'single_point' 'two_points' 'five_points' '10_points' '25_points' '50_points' '75_points' '100_points'
+    for REF in 'single_point' 'two_points' 'five_points' \
+    '10_points' '25_points' '50_points' '75_points' '100_points' \
+    'line' 'sheet' 'sphere' 'cylinder' \
+    'point_and_line' 'point_and_sheet' 'point_and_cylinder' \
+    'several_points_and_line' 'several_points_and_sheet' 'several_points_and_cylinder'
     do
       #python manager.py slurm test.py --partition abc_a100 --mem '500GB' --gpus 3 --cpus 16 \
       #python manager.py slurm test.py --partition abc --constraint titan --mem '500GB' --gpus 4 --cpus 20 \
