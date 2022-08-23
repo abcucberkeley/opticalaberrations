4e6bb950d61fbbed3599e0c230556c11ae984d89
Thayer <thayeralshaabi@gmail.com>
2022-08-23 10:49:55 -0700

    skip class split to speedup fileio for training

--- src/data_utils.py
+++ src/data_utils.py
@@ -80,35 +80,22 @@ def collect_dataset(
     distribution='/',
     samplelimit=None,
     max_amplitude=1.,
-):
-    train_data, val_data = None, None
-    classes = [
-        c for c in Path(datadir).rglob('*/')
-        if c.is_dir()
-           and len(list(c.glob('*.tif'))) > 0
-           and distribution in str(c)
-           and float(str([s for s in c.parts if s.startswith('amp_')][0]).split('-')[-1].replace('p', '.')) <= max_amplitude
-    ]
+): 
+    if split is not None:
+        train_data, val_data = None, None
+        classes = [
+            c for c in Path(datadir).rglob('*/')
+            if c.is_dir()
+               and len(list(c.glob('*.tif'))) > 0
+               and distribution in str(c)
+               and float(str([s for s in c.parts if s.startswith('amp_')][0]).split('-')[-1].replace('p', '.')) <= max_amplitude
+        ]
 
-    for c in classes:
-        if split is not None:
+        for c in classes:
             t, v = load_dataset(c, multiplier=multiplier, split=split, samplelimit=samplelimit)
             train_data = t if train_data is None else train_data.concatenate(t)
             val_data = v if val_data is None else val_data.concatenate(v)
-        else:
-            data = load_dataset(c, multiplier=multiplier, samplelimit=samplelimit)
-            train_data = data if train_data is None else train_data.concatenate(data)
 
-    if split is None:
-        train = train_data.map(lambda x: tf.py_function(get_sample, [x], [tf.float32, tf.float32]))
-        for img, y in train.take(1):
-            logger.info(f"Input: {img.numpy().shape}")
-            logger.info(f"Output: {y.numpy().shape}")
-
-        logger.info(f"Training samples: {tf.data.experimental.cardinality(train).numpy()}")
-        return train
-
-    else:
         train = train_data.map(lambda x: tf.py_function(get_sample, [x], [tf.float32, tf.float32]))
         val = val_data.map(lambda x: tf.py_function(get_sample, [x], [tf.float32, tf.float32]))
 
@@ -120,6 +107,18 @@ def collect_dataset(
         logger.info(f"Validation samples: {tf.data.experimental.cardinality(val).numpy()}")
 
         return train, val
+
+    else:
+        data = load_dataset(datadir, multiplier=multiplier, samplelimit=samplelimit)
+        data = data.map(lambda x: tf.py_function(get_sample, [x], [tf.float32, tf.float32]))
+
+        for img, y in data.take(1):
+            logger.info(f"Input: {img.numpy().shape}")
+            logger.info(f"Output: {y.numpy().shape}")
+
+        logger.info(f"Training samples: {tf.data.experimental.cardinality(data).numpy()}")
+
+        return data
 
 
 def create_dataset(config, split=None):
