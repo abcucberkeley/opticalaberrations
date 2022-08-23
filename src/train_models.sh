46896f6c172cac312fad9b874067d899fed5b130
Thayer <thayeralshaabi@gmail.com>
2022-08-23 15:40:48 -0700

    oops typos

--- src/train_models.sh
+++ src/train_models.sh
@@ -13,7 +13,7 @@ BATCH=512
 DATA='/clusterfs/nvme/thayer/allencell/aics/label-free-imaging-collection/dataset/train/golgi_apparatus/x150-y150-z600/'
 
 
-python manager.py slurm train.py --partition abc_a100 --nodes 4 --gpus_per_node 4 --cpus_per_gpu 4 --mem-per-gpu '125GB' \
+python manager.py slurm train.py --partition abc_a100 --nodes 4 --gpus_per_node 4 --cpus_per_gpu 4 --mem_per_gpu '125GB' \
 --task "--network opticaltransformer --cluster --opt Adamw --patch_size '8-8-8-8' --max_amplitude $MAXAMP --batch_size $BATCH --dataset $DATA/i$SHAPE --input_shape $SHAPE --depth_scalar $DEPTH --modes $MODES --x_voxel_size $xVOXEL --y_voxel_size $yVOXEL --z_voxel_size $zVOXEL" \
 --taskname p8x4 \
 --task "--network opticaltransformer --cluster  --opt Adamw --patch_size '32-16-8-8' --max_amplitude $MAXAMP --batch_size $BATCH --dataset $DATA/i$SHAPE --input_shape $SHAPE --depth_scalar $DEPTH --modes $MODES --x_voxel_size $xVOXEL --y_voxel_size $yVOXEL --z_voxel_size $zVOXEL" \
@@ -21,21 +21,21 @@ python manager.py slurm train.py --partition abc_a100 --nodes 4 --gpus_per_node 
 --name new/allencell/transformers
 
 #### Fourier-space models
-#python manager.py slurm train.py --partition abc_a100 --nodes 2 --gpus_per_node 4 --cpus_per_gpu 4 --mem-per-gpu '125GB' \
+#python manager.py slurm train.py --partition abc_a100 --nodes 2 --gpus_per_node 4 --cpus_per_gpu 4 --mem_per_gpu '125GB' \
 #--task "--network opticaltransformer --cluster --opt Adamw --patch_size '8-8-8-8' --max_amplitude $MAXAMP --batch_size $BATCH --dataset $DATA/i$SHAPE --input_shape $SHAPE --depth_scalar $DEPTH --modes $MODES --x_voxel_size $xVOXEL --y_voxel_size $yVOXEL --z_voxel_size $zVOXEL" \
 #--taskname p8x4 \
 #--task "--network opticaltransformer --cluster --opt Adamw --patch_size '32-16-8-8' --max_amplitude $MAXAMP --batch_size $BATCH --dataset $DATA/i$SHAPE --input_shape $SHAPE --depth_scalar $DEPTH --modes $MODES --x_voxel_size $xVOXEL --y_voxel_size $yVOXEL --z_voxel_size $zVOXEL" \
 #--taskname p32-p16-p8x2 \
 #--name new/allencell/transformers
 
-#python manager.py slurm train.py --partition abc_a100 --nodes 2 --gpus_per_node 4 --cpus_per_gpu 4 --mem-per-gpu '125GB' \
+#python manager.py slurm train.py --partition abc_a100 --nodes 2 --gpus_per_node 4 --cpus_per_gpu 4 --mem_per_gpu '125GB' \
 #--task "--network widekernel --cluster --mul --batch_size $BATCH --max_amplitude $MAXAMP --dataset $DATA/i$SHAPE --input_shape $SHAPE --depth_scalar $DEPTH --modes $MODES --x_voxel_size $xVOXEL --y_voxel_size $yVOXEL --z_voxel_size $zVOXEL" \
 #--taskname widekernel \
 #--task "--network opticalresnet --cluster --mul --batch_size $BATCH --max_amplitude $MAXAMP --dataset $DATA/i$SHAPE --input_shape $SHAPE --depth_scalar $DEPTH --modes $MODES --x_voxel_size $xVOXEL --y_voxel_size $yVOXEL --z_voxel_size $zVOXEL" \
 #--taskname opticalresnet \
 #--name new/allencell/convs
 
-#python manager.py slurm train.py --partition dgx --gpus_per_node 8 --cpus_per_gpu 16 --mem-per-gpu '240GB' \
+#python manager.py slurm train.py --partition dgx --gpus_per_node 8 --cpus_per_gpu 16 --mem_per_gpu '240GB' \
 #--task "--network opticaltransformer --opt Adamw --patch_size '32-16-8-4' --max_amplitude $MAXAMP --batch_size 256 --dataset $DATA/i$SHAPE --input_shape $SHAPE --depth_scalar $DEPTH --modes $MODES --x_voxel_size $xVOXEL --y_voxel_size $yVOXEL --z_voxel_size $zVOXEL" \
 #--taskname p32-p16-p8-p4 \
 #--name new/embeddings/transformers
