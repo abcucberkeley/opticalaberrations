c1560ee980bb2faf8940553a15cace9a5fce5ce2
Thayer <thayeralshaabi@gmail.com>
2022-09-27 13:09:12 -0700

    change slurm configs for eval

--- src/test_models.sh
+++ src/test_models.sh
@@ -20,21 +20,19 @@ for MODEL in "${models[@]}"
 do
   for NA in 1 .95 .9 .85 .8
   do
-    #python manager.py slurm test.py --partition abc_a100 --mem '500GB' --gpus 4 --cpus 16 \
-    #python manager.py slurm test.py --partition abc --constraint titan --mem '500GB' --gpus 4 --cpus 20 \
-    python manager.py slurm test.py --partition abc --mem '500GB' --cpus 24 --gpus 0 \
+    python manager.py slurm test.py --partition abc --mem '250GB' --cpus 12 --gpus 0 \
     --task "$MODEL $PHASE --datadir $DATA/i$SHAPE --n_samples $SAMPLES --na $NA --psf_type $PSF_TYPE --wavelength $LAMBDA --x_voxel_size $xVOXEL --y_voxel_size $yVOXEL --z_voxel_size $zVOXEL --max_amplitude $MAXAMP evalheatmap" \
     --taskname $NA \
     --name $MODEL/evalheatmaps_1.0
 
-    #python manager.py slurm test.py --partition abc --mem '500GB' --cpus 24 --gpus 0 \
+    #python manager.py slurm test.py --partition abc --mem '250GB' --cpus 12 --gpus 0 \
     #--task "$MODEL $PHASE --datadir $DATA/i$SHAPE --n_samples $SAMPLES --na $NA --psf_type $PSF_TYPE --wavelength $LAMBDA --x_voxel_size $xVOXEL --y_voxel_size $yVOXEL --z_voxel_size $zVOXEL --max_amplitude $MAXAMP iterheatmap" \
     #--taskname $NA \
     #--name $MODEL/iterheatmaps_1.0
 
     for N in 2 3 4 5
     do
-      python manager.py slurm test.py --partition abc --mem '500GB' --cpus 24 --gpus 0 \
+      python manager.py slurm test.py --partition abc --mem '250GB' --cpus 12 --gpus 0 \
       --task "$MODEL $PHASE --num_neighbor $N --datadir $DATA/i$SHAPE --n_samples $SAMPLES --na $NA --psf_type $PSF_TYPE --wavelength $LAMBDA --x_voxel_size $xVOXEL --y_voxel_size $yVOXEL --z_voxel_size $zVOXEL --max_amplitude $MAXAMP distheatmap" \
       --taskname $N \
       --name $MODEL/distheatmaps_neighbor_$N
