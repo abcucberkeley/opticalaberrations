d153438c13c340d030b4d6d4a6c22a0619d32f13
Thayer <thayeralshaabi@gmail.com>
2022-10-20 12:39:16 -0700

    eval new yumb z60 model

--- src/test_models.sh
+++ src/test_models.sh
@@ -24,30 +24,27 @@ do
   do
     for COV in 1.0 0.75 0.5
     do
-      python manager.py slurm test.py --partition abc --mem '250GB' --cpus 12 --gpus 0 \
+      #python manager.py slurm test.py --partition abc --mem '250GB' --cpus 12 --gpus 0 \
+#      python manager.py slurm test.py --partition abc_a100 --mem '500GB' --cpus 16 --gpus 0 \
+      python manager.py slurm test.py --partition dgx --mem '250GB' --cpus 16 --gpus 0 \
       --task "$MODEL --datadir $DATA/i$SHAPE --input_coverage $COV --modes $MODES --n_samples $SAMPLES --na $NA --psf_type $PSF_TYPE --wavelength $LAMBDA --x_voxel_size $xVOXEL --y_voxel_size $yVOXEL --z_voxel_size $zVOXEL --max_amplitude $MAXAMP evalheatmap" \
       --taskname $NA \
       --name $MODEL/evalheatmaps_${COV}
 
-      python manager.py slurm test.py --partition abc --mem '250GB' --cpus 12 --gpus 0 \
+      python manager.py slurm test.py --partition dgx --mem '250GB' --cpus 16 --gpus 0 \
       --task "$MODEL --datadir $DATA/i$SHAPE --input_coverage $COV --modes $MODES --n_samples $SAMPLES --na $NA --psf_type $PSF_TYPE --wavelength $LAMBDA --x_voxel_size $xVOXEL --y_voxel_size $yVOXEL --z_voxel_size $zVOXEL --max_amplitude $MAXAMP distheatmap" \
       --taskname $NA \
       --name $MODEL/distheatmaps_${COV}
 
-      python manager.py slurm test.py --partition abc --mem '250GB' --cpus 12 --gpus 0 \
+      python manager.py slurm test.py --partition dgx --mem '250GB' --cpus 16 --gpus 0 \
       --task "$MODEL --datadir $DATA/i$SHAPE --input_coverage $COV --modes $MODES --n_samples $SAMPLES --na $NA --psf_type $PSF_TYPE --wavelength $LAMBDA --x_voxel_size $xVOXEL --y_voxel_size $yVOXEL --z_voxel_size $zVOXEL --max_amplitude $MAXAMP densityheatmap" \
       --taskname $NA \
       --name $MODEL/densityheatmaps_${COV}
 
       python manager.py slurm test.py --partition abc_a100 --mem '500GB' --cpus 16 --gpus 0 \
-      --task "$MODEL --datadir $PSF_DATA/i$SHAPE --input_coverage $COV --modes $MODES --n_samples $SAMPLES --na $NA --psf_type $PSF_TYPE --wavelength $LAMBDA --x_voxel_size $xVOXEL --y_voxel_size $yVOXEL --z_voxel_size $zVOXEL --max_amplitude $MAXAMP iterheatmap" \
+      --task "$MODEL --datadir $DATA/i$SHAPE --input_coverage $COV --modes $MODES --n_samples $SAMPLES --na $NA --psf_type $PSF_TYPE --wavelength $LAMBDA --x_voxel_size $xVOXEL --y_voxel_size $yVOXEL --z_voxel_size $zVOXEL --max_amplitude $MAXAMP iterheatmap" \
       --taskname $NA \
       --name $MODEL/iterheatmaps_${COV}
-
-      #python manager.py slurm test.py --partition abc --mem '500GB' --cpus 24 --gpus 0 \
-      #--task "$MODEL --datadir $DATA/i$SHAPE --input_coverage $COV --modes $MODES --n_samples $SAMPLES --na $NA --psf_type $PSF_TYPE --wavelength $LAMBDA --x_voxel_size $xVOXEL --y_voxel_size $yVOXEL --z_voxel_size $zVOXEL --max_amplitude $MAXAMP evalpoints" \
-      #--taskname $NA \
-      #--name $MODEL/evalpoints_neighbor_None_${COV}
     done
 
     #for N in 2 3 4 5
