36a7dacd470b9254831a89bb5d0c752db0d865eb
dmilkie <dmilkie@gmail.com>
2022-11-22 10:32:51 -0500

    Autogenerate output folder

--- src/data.py
+++ src/data.py
@@ -59,6 +59,8 @@ def main(args=None):
     physical_devices = tf.config.list_physical_devices('GPU')
     for gpu_instance in physical_devices:
         tf.config.experimental.set_memory_growth(gpu_instance, True)
+       
+    Path('../data/').mkdir(parents=True, exist_ok=True) # add output directory if it doesn't exist
 
     if args.dtype == "shapes":
         shapes.simobjects()
