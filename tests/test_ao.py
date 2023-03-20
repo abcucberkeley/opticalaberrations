bcc92294f8358b61dec1316784282ee00dbe4eb3
Thayer <thayeralshaabi@gmail.com>
2023-03-20 10:58:06 -0700

    test_predict_tiles

--- tests/test_ao.py
+++ tests/test_ao.py
@@ -12,6 +12,8 @@ logger = logging.getLogger('')
 import math
 import pytest
 from pathlib import Path
+from operator import floordiv
+import numpy as np
 
 from src import experimental
 from src import preprocessing
@@ -21,12 +23,20 @@ from src import preprocessing
 def kargs():
     repo = Path.cwd()
     num_modes = 15
+    input_shape = (256, 256, 256)  # z-y-x
+    window_size = (64, 64, 64)  # z-y-x
+    num_tiles = np.prod(tuple(map(floordiv, input_shape, window_size)))
+    digital_rotations = range(0, 361)
+
     kargs = dict(
         inputs=repo / f'examples/single/single.tif',
-        input_shape=(256, 256, 256),
+        input_shape=input_shape,
         embeddings_shape=(6, 64, 64, 1),
-        digital_rotations=range(0, 361),
-        rotations_shape=(361, 6, 64, 64, 1),
+        digital_rotations=digital_rotations,
+        rotations_shape=(len(digital_rotations), 6, 64, 64, 1),
+        window_size=window_size,
+        num_tiles=num_tiles,
+        tiles_shape=(num_modes, num_tiles+5),
         num_modes=num_modes,
         zernikes_shape=(num_modes, 3),
         model=repo / f'pretrained_models/lattice_yumb_x108um_y108um_z200um/opticalnet-{num_modes}.h5',
@@ -43,11 +53,20 @@ def kargs():
         freq_strength_threshold=.01,
         prediction_threshold=0.,
         num_predictions=1,
-        window_size='64-64-64',  # z-y-x
         batch_size=128,
         plot=True,
         plot_rotations=True,
         ignore_modes=[],
+        # extra `aggregate_predictions` flags
+        majority_threshold=.5,
+        min_percentile=1,
+        max_percentile=99,
+        final_prediction='mean',
+        ignore_tile=[],
+        # extra `predict_rois` flags
+        num_rois=10,
+        min_intensity=200,
+        minimum_distance=.5,
     )
 
     return kargs
@@ -144,6 +163,7 @@ def test_phase_retrieval(kargs):
         wavelength=kargs['wavelength'],
         ignore_modes=kargs['ignore_modes'],
         plot=kargs['plot'],
+        prediction_threshold=kargs['prediction_threshold'],
     )
     assert zernikes.shape == kargs['zernikes_shape']
 
@@ -162,6 +182,7 @@ def test_predict_sample(kargs):
         plot_rotations=kargs['plot'],
         batch_size=kargs['batch_size'],
         ignore_modes=kargs['ignore_modes'],
+        prediction_threshold=kargs['prediction_threshold'],
     )
     assert zernikes.shape == kargs['zernikes_shape']
 
@@ -180,5 +201,43 @@ def test_predict_large_fov(kargs):
         plot_rotations=kargs['plot'],
         batch_size=kargs['batch_size'],
         ignore_modes=kargs['ignore_modes'],
+        prediction_threshold=kargs['prediction_threshold'],
     )
+    assert zernikes.shape == kargs['zernikes_shape']
+
+
+def test_predict_tiles(kargs):
+    tile_predictions = experimental.predict_tiles(
+        model=kargs['model'],
+        img=kargs['inputs'],
+        prev=kargs['prev'],
+        axial_voxel_size=kargs['axial_voxel_size'],
+        lateral_voxel_size=kargs['lateral_voxel_size'],
+        wavelength=kargs['wavelength'],
+        plot=kargs['plot'],
+        plot_rotations=kargs['plot'],
+        batch_size=kargs['batch_size'],
+        ignore_modes=kargs['ignore_modes'],
+        window_size=kargs['window_size'],
+    )
+
+    assert tile_predictions.shape == kargs['tiles_shape']
+
+    zernikes = experimental.aggregate_predictions(
+        model=kargs['model'],
+        model_pred=Path(f"{kargs['inputs'].with_suffix('')}_tiles_predictions.csv"),
+        dm_calibration=kargs['dm_calibration'],
+        dm_state=kargs['dm_state'],
+        axial_voxel_size=kargs['axial_voxel_size'],
+        lateral_voxel_size=kargs['lateral_voxel_size'],
+        wavelength=kargs['wavelength'],
+        prediction_threshold=kargs['prediction_threshold'],
+        majority_threshold=kargs['majority_threshold'],
+        min_percentile=kargs['min_percentile'],
+        max_percentile=kargs['max_percentile'],
+        final_prediction=kargs['final_prediction'],
+        ignore_tile=kargs['ignore_tile'],
+        plot=kargs['plot'],
+    )
+
     assert zernikes.shape == kargs['zernikes_shape']
