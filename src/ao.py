c8c2e84aa757b15a777a71b11606723871e84be0
Aang <dmilkie@gmail.com>
2023-03-24 09:09:29 -0700

    DM plotting info

--- src/ao.py
+++ src/ao.py
@@ -492,7 +492,7 @@ def parse_args(args):
         help="Evaluate artificially introduced aberrations via the DM"
     )
     eval_dataset.add_argument("datadir", type=Path, help="path to dataset directory")
-    eval_dataset.add_argument("--flat", default=None, type=Path, help="path to the flat DM acts file")
+    eval_dataset.add_argument("--flat", default=None, type=Path, help="path to the flat DM acts file. If this is given, then DM surface plots will be made.")
     eval_dataset.add_argument("--skip_eval_plots", action='store_true', help="skip generating the _ml_eval.svg files.")
     eval_dataset.add_argument("--precomputed", action='store_true')
     eval_dataset.add_argument(
