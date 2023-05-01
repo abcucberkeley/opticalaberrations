f8fd2fceb403c820559839df5e95308cf603664d
Aang <dmilkie@gmail.com>
2023-05-01 12:23:24 -0700

    Rip out print debugs

--- src/backend.py
+++ src/backend.py
@@ -466,12 +466,6 @@ def eval_rotation(
                 unconfident         0           0
             """
 
-            if 'z1-y27-x3' in str(save_path):
-                print('we are here')
-                print(rhos)
-                print(std_rho)
-
-
             if np.allclose(rhos, rhos[0], atol=0.0001):  # blank image (unconfident)
                 preds[mode.index_ansi], preds[twin.index_ansi] = 0., 0.
                 stdevs[mode.index_ansi], stdevs[twin.index_ansi] = 0., 0.
@@ -522,9 +516,7 @@ def eval_rotation(
             df['pred_twin_angle'] = np.nan
             df['fitted_twin_angle'] = np.nan
             df['mse'] = np.nan
-        if 'z1-y27-x3' in str(save_path):
-            print(f'{confident=}')
-            print(np.isclose(rhos, rhos[0], atol=0.0001))
+
 
         df['confident'] = confident
         df['aggr_rho'] = rho
