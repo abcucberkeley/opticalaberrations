631aaaf0bbc672911fb61baeb1c804d382a08482
Aang <dmilkie@gmail.com>
2023-05-09 11:04:46 -0700

    Fix parsing strings for cluster

--- src/ao.py
+++ src/ao.py
@@ -761,6 +761,7 @@ def main(args=None, preloaded: Preloadedmodelclass = None):
         flags = flags.replace("..", cluster_repo)       # regex stinks at replacing ".."
         flags = re.sub(pattern='~/nvme2', repl='/clusterfs/nvme2/', string=flags)
         flags = re.sub(pattern='U:\\\\', repl='/clusterfs/nvme2/', string=flags)
+        flags = re.sub(pattern='U:/', repl='/clusterfs/nvme2/', string=flags)
         flags = re.sub(pattern='--batch_size \d+', repl='--batch_size 3500', string=flags)
         taskname = f"{args.func}_{args.input.stem}"
 
