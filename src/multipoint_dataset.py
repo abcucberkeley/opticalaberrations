fix unit conversion

src/multipoint_dataset.py
@@ -21,7 +21,7 @@ import matplotlib.pyplot as plt
 plt.set_loglevel('error')
 
 import cli
-from utils import mean_min_distance, randuniform, add_noise
+from utils import mean_min_distance, randuniform, add_noise, electrons2counts
 from preprocessing import prep_sample, resize_with_crop_or_pad
 from utils import fftconvolution, multiprocess
 from synthetic import SyntheticPSF
@@ -187,7 +187,7 @@ def sim(
             electrons_per_count=electrons_per_count,
         )
     else:  # convert image to counts
-        inputs = img / electrons_per_count
+        inputs = electrons2counts(img, electrons_per_count=electrons_per_count)
 
     maxcounts = np.max(inputs)
 
@@ -200,7 +200,7 @@ def sim(
             odir = outdir/e
             odir.mkdir(exist_ok=True, parents=True)
 
-            inputs = prep_sample(
+            embeddings = prep_sample(
                 inputs,
                 sample_voxel_size=gen.voxel_size,
                 model_fov=gen.psf_fov,
@@ -213,7 +213,7 @@ def sim(
             )
 
             embeddings = np.squeeze(fourier_embeddings(
-                inputs=inputs,
+                inputs=embeddings,
                 iotf=gen.iotf,
                 embedding_option=e,
                 alpha_val=alpha_val,

src/psf_dataset.py
@@ -20,7 +20,7 @@ import matplotlib.pyplot as plt
 plt.set_loglevel('error')
 
 import cli
-from utils import multiprocess, add_noise, randuniform
+from utils import multiprocess, add_noise, randuniform, electrons2counts
 from synthetic import SyntheticPSF
 from wavefront import Wavefront
 
@@ -114,7 +114,7 @@ def sim(
             electrons_per_count=electrons_per_count,
         )
     else:  # convert image to counts
-        inputs = kernel / electrons_per_count
+        inputs = electrons2counts(kernel, electrons_per_count=electrons_per_count)
 
     maxcounts = np.max(inputs)
 

src/utils.py
@@ -15,6 +15,7 @@ from astropy import convolution
 import multiprocessing as mp
 from line_profiler_pycharm import profile
 from typing import Any, List, Union, Optional, Generator
+from scipy import constants
 
 from preprocessing import resize_with_crop_or_pad
 from wavefront import Wavefront
@@ -69,6 +70,22 @@ def multiprocess(
     return np.array(logs)
 
 
+def photons2electrons(image, quantum_efficiency: float = .82):
+    return image * quantum_efficiency
+
+
+def electrons2photons(image, quantum_efficiency: float = .82):
+    return image / quantum_efficiency
+
+
+def electrons2counts(image, electrons_per_count: float = .22):
+    return image / electrons_per_count
+
+
+def counts2electrons(image, electrons_per_count: float = .22):
+    return image * electrons_per_count
+
+
 def randuniform(var):
     """
     Returns a random number (uniform chance) in the range provided by var. If var is a scalar, var is simply returned.
@@ -118,13 +135,13 @@ def add_noise(
     """
     sigma_background_noise *= electrons_per_count  # electrons;  40 counts = 40 * .22 electrons per count
     dark_read_noise = normal_noise(mean=0, sigma=sigma_background_noise, size=image.shape)  # dark image in electrons
-    shot_noise = poisson_noise(image=image) / quantum_efficiency  # convert shot noise to electrons
+    shot_noise = photons2electrons(poisson_noise(image=image), quantum_efficiency=quantum_efficiency)
 
-    image *= quantum_efficiency             # convert image from photons to electrons
+    image = photons2electrons(image, quantum_efficiency=quantum_efficiency)
     image += shot_noise + dark_read_noise
-    image /= electrons_per_count            # convert image to counts
+    image = electrons2counts(image, electrons_per_count=electrons_per_count)
 
-    image += mean_background_offset         # add camera offset
+    image += mean_background_offset    # add camera offset (camera offset in counts)
     image[image < 0] = 0
     return image
 