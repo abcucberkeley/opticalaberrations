67625952631a00ab43ad392733adfa2e5c36c7c0
Thayer <thayeralshaabi@gmail.com>
2023-05-24 15:56:52 -0700

    fix path

--- src/test_models.sh
+++ src/test_models.sh
@@ -17,15 +17,15 @@ for EVALSIGN in positive_only signed
 do
   for MODES in 15 28 45
   do
-      MODEL="$PRETRAINED/opticalnet-$MODES.h5"
+      MODEL="$PRETRAINED/opticalnet-$MODES"
 
       python manager.py slurm test.py --partition abc --constraint 'titan' --mem '125GB' --cpus 5 --gpus 1 \
-      --task "$MODEL --eval_sign $EVALSIGN --digital_rotations random" \
+      --task "$MODEL.h5 --eval_sign $EVALSIGN --digital_rotations random" \
       --taskname random \
       --name $MODEL/$EVALSIGN/samples
 
       python manager.py slurm test.py --partition abc --constraint 'titan' --mem '125GB' --cpus 5 --gpus 1 \
-      --task "$MODEL --eval_sign $EVALSIGN --digital_rotations modes" \
+      --task "$MODEL.h5 --eval_sign $EVALSIGN --digital_rotations modes" \
       --taskname evalmodes \
       --name $MODEL/$EVALSIGN/evalmodes
 
@@ -34,17 +34,17 @@ do
 #        for COV in 1.0
 #        do
 #          python manager.py slurm test.py --partition abc_a100 --mem '500GB' --cpus 16 --gpus 4 \
-#          --task "$MODEL --datadir $DATA --input_coverage $COV --na $NA --batch_size $BATCH --niter $ITERS --eval_sign $EVALSIGN --n_samples $MAX densityheatmap" \
+#          --task "$MODEL.h5 --datadir $DATA --input_coverage $COV --na $NA --batch_size $BATCH --niter $ITERS --eval_sign $EVALSIGN --n_samples $MAX densityheatmap" \
 #          --taskname $NA \
 #          --name $MODEL/$EVALSIGN/densityheatmaps_${COV}
 #
 #          python manager.py slurm test.py --partition abc_a100 --mem '500GB' --cpus 16 --gpus 4 \
-#          --task "$MODEL --datadir $DATA --input_coverage $COV --na $NA --batch_size $BATCH  --niter 10 --eval_sign $EVALSIGN --n_samples $MAX iterheatmap" \
+#          --task "$MODEL.h5 --datadir $DATA --input_coverage $COV --na $NA --batch_size $BATCH  --niter 10 --eval_sign $EVALSIGN --n_samples $MAX iterheatmap" \
 #          --taskname $NA \
 #          --name $MODEL/$EVALSIGN/iterheatmaps_${COV}
 #
 #          python manager.py slurm test.py --partition dgx --mem '500GB' --cpus 32 --gpus 2 \
-#          --task "$MODEL --datadir $DATA --input_coverage $COV --na $NA --batch_size $BATCH --niter $ITERS --eval_sign $EVALSIGN --n_samples $MAX snrheatmap" \
+#          --task "$MODEL.h5 --datadir $DATA --input_coverage $COV --na $NA --batch_size $BATCH --niter $ITERS --eval_sign $EVALSIGN --n_samples $MAX snrheatmap" \
 #          --taskname $NA \
 #          --name $MODEL/$EVALSIGN/snrheatmaps_${COV}
 #        done
