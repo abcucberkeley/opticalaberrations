94e003df3962cc36b59dd2aac1e7e49147896790
Thayer <thayeralshaabi@gmail.com>
2023-06-23 14:40:08 -0700

    training new models

--- src/train_fourierspace_models.sh
+++ src/train_fourierspace_models.sh
@@ -12,14 +12,14 @@ DEFOCUS_ONLY='--defocus_only'
 EMB="spatial_planes"
 DATASET='new_embeddings'
 DATA="/clusterfs/nvme/thayer/dataset/new_voxelsize/train/x97-y97-z200/"
-BATCH=4096
+BATCH=2048
 NETWORK=opticalnet
 
-for EMB in spatial_planes1020
+for EMB in spatial_planes spatial_planes1020
 do
   for MODES in 15
   do
-    python multinode_manager.py train.py --partition abc_a100 --mem '500GB' --nodes 4 --gpus 4 --cpus 16 \
+    python multinode_manager.py train.py --partition abc_a100 --mem '500GB' --nodes 2 --gpus 4 --cpus 16 \
     --task "--multinode --network $NETWORK --embedding $EMB --patch_size '32-16-8-8' --modes $MODES --max_amplitude $MAXAMP --batch_size $BATCH --dataset $DATA/i$SHAPE/z$MODES --input_shape $SHAPE --depth_scalar $DEPTH" \
     --taskname $NETWORK \
     --name new/$DATASET/$NETWORK-$MODES-$EMB
