33a4887eae5acecfdd7f5bb7f314993cf685d957
Thayer <thayeralshaabi@gmail.com>
2023-08-02 14:09:20 -0700

    update file permissions

--- src/ao.py
+++ src/ao.py
@@ -1130,6 +1130,9 @@ def main(args=None, preloaded: Preloadedmodelclass = None):
 
         logger.info(f"Total time elapsed: {time.time() - timeit:.2f} sec.")
 
+        logger.info(f"Updating file permissions to {args.input.parent}")
+        subprocess.run(f"setfacl -PRdm u::rwx,g::rwx,o::rwwx {str(Path(args.input).parent.resolve())}", shell=True)
+
 
 if __name__ == "__main__":
     main()
