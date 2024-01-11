4439db137902a2cfa8cfb17b94dd751f992acf87
Milkie <dmilkie@gmail.com>
2024-01-11 11:50:36 -0500

    Change order of tests in test_ao.py
    Change order in test_ao.py to (hopefully) avoid predict_tiles from failing.  Order won't be used unless pytest-order is installed.

--- tests/test_ao.py
+++ tests/test_ao.py
@@ -17,23 +17,48 @@ from src import experimental
 
 
 @pytest.mark.run(order=1)
-def test_phase_retrieval(kargs):
-    zernikes = experimental.phase_retrieval(
+def test_predict_tiles(kargs):
+    logging.info(f"Pytest will assert that 'tile_predictions' has output shape of: "
+                 f"(num_modes={kargs['num_modes']}, num_tiles={kargs['num_tiles']}), "
+                 f"since window_size={kargs['window_size']}.")
+    tile_predictions = experimental.predict_tiles(
+        model=kargs['model'],
         img=kargs['inputs'],
-        num_modes=kargs['num_modes'],
+        prev=kargs['prev'],
         dm_calibration=kargs['dm_calibration'],
         dm_state=kargs['dm_state'],
         axial_voxel_size=kargs['axial_voxel_size'],
         lateral_voxel_size=kargs['lateral_voxel_size'],
         wavelength=kargs['wavelength'],
-        ignore_modes=kargs['ignore_modes'],
         plot=kargs['plot'],
-        prediction_threshold=kargs['prediction_threshold'],
+        plot_rotations=kargs['plot_rotations'],
+        batch_size=kargs['batch_size'],
+        ignore_modes=kargs['ignore_modes'],
+        window_size=kargs['window_size'],
+        min_psnr=kargs['min_psnr']
     )
-    assert zernikes.shape[0] == kargs['num_modes']
+    tile_predictions = tile_predictions.drop(columns=['mean', 'median', 'min', 'max', 'std'])
+    assert tile_predictions.shape == (kargs['num_modes'], kargs['num_tiles']), f'{tile_predictions=}'
 
 
 @pytest.mark.run(order=2)
+def test_aggregate_tiles(kargs):
+    zernikes = experimental.aggregate_predictions(
+        model_pred=Path(f"{kargs['inputs'].with_suffix('')}_tiles_predictions.csv"),
+        dm_calibration=kargs['dm_calibration'],
+        dm_state=kargs['dm_state'],
+        prediction_threshold=kargs['prediction_threshold'],
+        majority_threshold=kargs['majority_threshold'],
+        min_percentile=kargs['min_percentile'],
+        max_percentile=kargs['max_percentile'],
+        aggregation_rule=kargs['aggregation_rule'],
+        max_isoplanatic_clusters=kargs['max_isoplanatic_clusters'],
+        ignore_tile=kargs['ignore_tile'],
+        plot=kargs['plot'],
+    )
+    assert zernikes.shape[1] == kargs['num_modes'] + 2
+
+@pytest.mark.run(order=3)
 def test_predict_sample(kargs):
     zernikes = experimental.predict_sample(
         model=kargs['model'],
@@ -53,8 +78,24 @@ def test_predict_sample(kargs):
     )
     assert zernikes.shape[0] == kargs['num_modes']
 
+@pytest.mark.run(order=4)
+def test_phase_retrieval(kargs):
+    zernikes = experimental.phase_retrieval(
+        img=kargs['inputs'],
+        num_modes=kargs['num_modes'],
+        dm_calibration=kargs['dm_calibration'],
+        dm_state=kargs['dm_state'],
+        axial_voxel_size=kargs['axial_voxel_size'],
+        lateral_voxel_size=kargs['lateral_voxel_size'],
+        wavelength=kargs['wavelength'],
+        ignore_modes=kargs['ignore_modes'],
+        plot=kargs['plot'],
+        prediction_threshold=kargs['prediction_threshold'],
+    )
+    assert zernikes.shape[0] == kargs['num_modes']
 
-@pytest.mark.run(order=3)
+
+@pytest.mark.run(order=5)
 def test_predict_large_fov(kargs):
     zernikes = experimental.predict_large_fov(
         model=kargs['model'],
@@ -75,7 +116,7 @@ def test_predict_large_fov(kargs):
     assert zernikes.shape[0] == kargs['num_modes']
 
 
-@pytest.mark.run(order=4)
+@pytest.mark.run(order=6)
 def test_predict_folder(kargs):
     test_folder = Path(f"{kargs['repo']}/dataset/experimental_zernikes/psfs")
     number_of_files = len(sorted(test_folder.glob(kargs['prediction_filename_pattern'])))
@@ -97,51 +138,5 @@ def test_predict_folder(kargs):
         ignore_modes=kargs['ignore_modes'],
         min_psnr=kargs['min_psnr']
     )
-
     predictions = predictions.drop(columns=['mean', 'median', 'min', 'max', 'std'])
     assert predictions.shape == (kargs['num_modes'], number_of_files)
-
-
-@pytest.mark.run(order=5)
-def test_predict_tiles(kargs):
-    logging.info(f"Pytest will assert that 'tile_predictions' has output shape of: "
-                 f"(num_modes={kargs['num_modes']}, num_tiles={kargs['num_tiles']}), "
-                 f"since window_size={kargs['window_size']}.")
-    tile_predictions = experimental.predict_tiles(
-        model=kargs['model'],
-        img=kargs['inputs'],
-        prev=kargs['prev'],
-        dm_calibration=kargs['dm_calibration'],
-        dm_state=kargs['dm_state'],
-        axial_voxel_size=kargs['axial_voxel_size'],
-        lateral_voxel_size=kargs['lateral_voxel_size'],
-        wavelength=kargs['wavelength'],
-        plot=kargs['plot'],
-        plot_rotations=kargs['plot_rotations'],
-        batch_size=kargs['batch_size'],
-        ignore_modes=kargs['ignore_modes'],
-        window_size=kargs['window_size'],
-        min_psnr=kargs['min_psnr']
-    )
-
-    tile_predictions = tile_predictions.drop(columns=['mean', 'median', 'min', 'max', 'std'])
-    assert tile_predictions.shape == (kargs['num_modes'], kargs['num_tiles']), f'{tile_predictions=}'
-
-
-@pytest.mark.run(order=6)
-def test_aggregate_tiles(kargs):
-    zernikes = experimental.aggregate_predictions(
-        model_pred=Path(f"{kargs['inputs'].with_suffix('')}_tiles_predictions.csv"),
-        dm_calibration=kargs['dm_calibration'],
-        dm_state=kargs['dm_state'],
-        prediction_threshold=kargs['prediction_threshold'],
-        majority_threshold=kargs['majority_threshold'],
-        min_percentile=kargs['min_percentile'],
-        max_percentile=kargs['max_percentile'],
-        aggregation_rule=kargs['aggregation_rule'],
-        max_isoplanatic_clusters=kargs['max_isoplanatic_clusters'],
-        ignore_tile=kargs['ignore_tile'],
-        plot=kargs['plot'],
-    )
-
-    assert zernikes.shape[1] == kargs['num_modes'] + 2
