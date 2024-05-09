d824fad5ef752887e81de413021c8761af5013c0
Milkie <dmilkie@gmail.com>
2024-05-09 14:43:51 -0400

    Github actions

--- tests/test_pytorch.py
+++ tests/test_pytorch.py
@@ -15,16 +15,18 @@ import pytest
 def test_pytorch(kargs):
 
     logging.info(f'Pytorch version = {torch.__version__}')
-    print(f'\n\nPytorch version = {torch.__version__}\n')
-
-    physical_devices = torch.cuda.device_count()
 
     gpu_workers = torch.cuda.device_count()
 
     if gpu_workers > 0:
-        gpu_model = torch.cuda.get_device_name(0)
+        for i in range(gpu_workers):
+            gpu_model = torch.cuda.get_device_name(i)
+            logging.info(f'Number of active GPUs: {gpu_workers}, {gpu_model}')
+        logging.info(f'GPU memory free : {torch.cuda.mem_get_info()[0] / (1024*1024*1024):0.1f} GB')
+        logging.info(f'GPU memory total: {torch.cuda.mem_get_info()[1] / (1024*1024*1024):0.1f} GB')
     else:
         gpu_model = None
+        logging.info(f'Number of active GPUs: {gpu_workers}, {gpu_model}')
 
-    print(f'Number of active GPUs: {gpu_workers}, {gpu_model}')
+
     assert gpu_workers > 0
